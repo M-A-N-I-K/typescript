12d9b1518dc8b30b1931ff5b70594993ebb03282
Manik Dingra <manikdhingra0582@gmail.com>
2023-09-06 04:08:48 +0530

    Generic types in react learned

--- react_typescript/src/App.tsx
+++ react_typescript/src/App.tsx
@@ -1,7 +1,8 @@
+import { useState } from "react";
 import Heading from "./components/Heading";
 import Section from "./components/Section";
 import Counter from "./components/Counter";
-import { useState } from "react";
+import List from "./components/List";
 
 const App = () => {
 	const [count, setCount] = useState<number>(1);
@@ -11,6 +12,10 @@ const App = () => {
 			<Heading title={"Hello"} />
 			<Section title={"Different Title"}>This is my Section</Section>
 			<Counter setCount={setCount}>Count is {count}</Counter>
+			<List
+				items={["Coffee", "Anime", "Code", "Manga"]}
+				render={(item: string) => <span className="bold">{item}</span>}
+			/>
 		</>
 	);
 };
