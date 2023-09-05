8121d5e1d6e1201a7d5552866a761e748d516bfc
Manik Dingra <manikdhingra0582@gmail.com>
2023-09-06 03:56:46 +0530

    Use state with type learned

--- react_typescript/src/App.tsx
+++ react_typescript/src/App.tsx
@@ -1,7 +1,18 @@
 import Heading from "./components/Heading";
+import Section from "./components/Section";
+import Counter from "./components/Counter";
+import { useState } from "react";
 
 const App = () => {
-	return <Heading title={"Hello"} />;
+	const [count, setCount] = useState<number>(1);
+
+	return (
+		<>
+			<Heading title={"Hello"} />
+			<Section title={"Different Title"}>This is my Section</Section>
+			<Counter setCount={setCount}>Count is {count}</Counter>
+		</>
+	);
 };
 
 export default App;
